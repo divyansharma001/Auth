06c16b54b9291efd57251b0174ba65eff853ebfb
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-10-19 12:16:11 +0530

    Add next-auth package and initialize the database schema

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,59 @@
 import NextAuth from "next-auth"
- 
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials"
+import { compare } from 'bcryptjs'
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Please provide both email and password")
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email as string }
+        })
+
+        if (!user || !user.password) {
+          throw new Error("Invalid credentials")
+        }
+
+      
+        const isPasswordValid = await compare(credentials.password as string, user.password)
+
+        if (!isPasswordValid) {
+          throw new Error("Invalid credentials")
+        }
+
+        return {
+          id: user.id.toString(),
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email,
+          password: user.password,
+          address: user.address,
+          phone: user.phone
+        }
+      },
+    })
+  ],
+  pages: {
+    signIn: '/auth/signin',
+    error: '/auth/error',
+  },
 })
