75dc699640d5ea50166765f5e20981c3523ae4fe
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-10-21 00:14:37 +0530

    Made bit resposnsive

--- src/components/Typewriter.tsx
+++ src/components/Typewriter.tsx
@@ -21,15 +21,15 @@ export function TypewriterEffectSmoothDemo() {
     },
   ];
   return (
-    <div className="flex flex-col items-center justify-center h-[30rem]  ">
-      <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
+    <div className="flex flex-col items-center justify-center sm:h-[30rem] h-[17rem] pt-10 sm:pt-0">
+      <p className="text-neutral-600 dark:text-neutral-200 text-md sm:text-base  ">
         Got you covered for you next projects.
       </p>
       <TypewriterEffectSmooth words={words} />
-      <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base pb-10 ">
+      <p className="text-neutral-600 dark:text-neutral-200 text-md sm:text-base pb-10 text-center">
        Just clone the repo, and you&apos;re done with the auth part.
       </p>
-      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 ">
         <Link href="https://github.com/divyansharma001/Auth">
         <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
           CodeBase
