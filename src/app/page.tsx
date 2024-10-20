0088ac79e194d762d791d3bdbf61cd3bcb5ef5b0
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-10-20 19:48:36 +0530

    Added secure routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { auth } from "@/auth";
-
-
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const session = await auth();
-  console.log(session?.user);
+  console.log(session);
+
+  if(!session){
+    redirect("/login")
+  }
 
   return (
    
