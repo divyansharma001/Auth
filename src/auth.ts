173abf24ff8817591fa23efb46e77935b5ac8bb7
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-10-20 19:27:03 +0530

    Auth err

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,4 @@
-import NextAuth, { AuthError } from "next-auth"
+import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import CredentialsProvider from "next-auth/providers/credentials"
 import { compare } from 'bcryptjs'
