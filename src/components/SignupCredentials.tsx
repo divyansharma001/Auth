5f2a4756a01b078d85a4ec95ad155d0bb5a92a88
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-10-21 00:30:19 +0530

    Added O auth in signup page

--- src/components/SignupCredentials.tsx
+++ src/components/SignupCredentials.tsx
@@ -10,6 +10,10 @@ import { motion } from "framer-motion";
 import { Poppins, Roboto } from 'next/font/google';
 import { signupAction } from '@/app/actions/signupAction';
 import { toast } from "sonner";
+import signUpImage from '@/images/signupImage.jpg'
+import { Github, Mail } from "lucide-react";
+import { githubLogin } from "@/app/actions/githubLogin";
+import { googleLogin } from "@/app/actions/googleLogin";
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -52,7 +56,7 @@ const SignupCredentials = () => {
         <div className="w-1/2 hidden md:block relative">
           <Image 
             alt='Welcome image' 
-            src={image} 
+            src={signUpImage} 
             layout="fill" 
             objectFit="cover"
             className="transition-all duration-300 transform hover:scale-105"
@@ -128,6 +132,42 @@ const SignupCredentials = () => {
               <BottomGradient />
             </motion.button>
           </form>
+
+          <div className="mt-6">
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t border-gray-300" />
+              </div>
+              <div className="relative flex justify-center text-sm">
+                <span className="px-2 bg-white text-gray-500">
+                  Or continue with
+                </span>
+              </div>
+            </div>
+
+            <div className="mt-6 flex justify-between space-x-4">
+              <form action={googleLogin}> 
+              <motion.button
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+                className="flex-1 flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                <Mail className="w-5 h-5 mr-2 text-red-500" />
+                Google
+              </motion.button>
+              </form>
+              <form action={githubLogin}>
+              <motion.button
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+                className="flex-1 flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                <Github className="w-5 h-5 mr-2" />
+                GitHub
+              </motion.button>
+              </form>
+            </div>
+          </div>
 
           <motion.div 
             initial={{ opacity: 0, y: 20 }}
